bc79421ea3ca8db54184380d8538f5e1be2761c4
roq-ai <>
2023-06-29 15:37:33 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,102 @@
+import { Button, Flex, Heading, Image, Text, Stack, useBreakpointValue, Box, Link } from '@chakra-ui/react';
+
+import { signIn, signUp, requireNextAuth } from '@roq/nextjs';
+
+import Head from 'next/head';
+import { HelpBox } from 'components/help-box';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>{`Sussybike`}</title>
+
+        <meta
+          name="description"
+          content="Welcome to Sussybike - Streamlining HR processes for enhanced efficiency and employee experience. Centralized data management, time tracking, performance evaluation, and engagement tools at your fingertips. Boosting productivity and organizational success through automation."
+        />
+      </Head>
+
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack position="relative" spacing={6} w={'full'} maxW={'lg'}>
+            <HelpBox />
+            <Image src="/roq.svg" alt="Logo" w="150px" mb="8" />
+            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+              <Text as={'span'}>Explore</Text>{' '}
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: useBreakpointValue({ base: '20%', md: '30%' }),
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'cyan.400',
+                  zIndex: -1,
+                }}
+              >
+                {`Sussybike`}
+              </Text>
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+              {`Welcome to Sussybike - Streamlining HR processes for enhanced efficiency and employee experience. Centralized data management, time tracking, performance evaluation, and engagement tools at your fingertips. Boosting productivity and organizational success through automation.`}
+            </Text>
+            <Stack direction="column" spacing={4} className="roles-container">
+              <Stack className="owner-roles-container" direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'cyan.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'cyan.700',
+                  }}
+                  onClick={() => signUp('owner')}
+                >
+                  Create Account
+                </Button>
+                <Button rounded={'full'} onClick={() => signIn('owner')}>
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex position="relative" flex={1}>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1529651737248-dad5e287768e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjA3NjB8MHwxfHNlYXJjaHwxfHxIUiUyMG1hbmFnZW1lbnQlMkNwcm9kdWN0aXZpdHl8ZW58MHx8fHwxNjg4MDUyOTgwfDA&ixlib=rb-4.0.3&q=80&w=1080'
+            }
+            alt={'Login Image'}
+            objectFit={'cover'}
+          />
+          <Box position="absolute" top="0" backgroundColor="rgba(0,0,0,0.6)" width="100%" py="2">
+            <Text align="center" fontSize="sm" color="white">
+              Photo by{' '}
+              <Link
+                href="https://unsplash.com/es/@bajkorenata?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >{`Renáta-Adrienn`}</Link>{' '}
+              on{' '}
+              <Link
+                href="https://unsplash.com/?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >
+                Unsplash
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
+      </Stack>
+    </>
+  );
+}
+
+export default requireNextAuth({
+  redirectIfAuthenticated: true,
+  redirectTo: '/users',
+})(HomePage);
---
+++ src/validationSchema/performance-evaluations/index.ts
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+export const performanceEvaluationValidationSchema = yup.object().shape({
+  evaluation_score: yup.number().integer().required(),
+  user_id: yup.string().nullable(),
+});
